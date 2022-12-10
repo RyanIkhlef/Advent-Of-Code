fbf508c601ed1ae6ed7034df825f9665f30f598c
Ryan <ryan.ikhlef59259@laposte.net>
2022-12-11 00:25:25 +0100

    Day 6 - both steps

--- day6.py
+++ day6.py
@@ -1,4 +1,6 @@
 input = "./inputs/day6/step_1"
+
+
 def step1():
     start_of_packet = 4
     is_found = False
@@ -8,7 +10,7 @@ def step1():
             line_size = len(line)
             index = 0
             while index < line_size:
-                marker = line[index:index+start_of_packet]
+                marker = line[index:index + start_of_packet]
                 for letter in marker:
                     if marker.count(letter) > 1:
                         index += 1
@@ -19,10 +21,11 @@ def step1():
                     start_of_packet += index
                     break
 
-
     print("La réponse de l'étape 1 est :", start_of_packet)
 
+
 step1()
+
 
 def step2():
     start_of_message = 14
@@ -33,7 +36,7 @@ def step2():
             line_size = len(line)
             index = 0
             while index < line_size:
-                marker = line[index:index+start_of_message]
+                marker = line[index:index + start_of_message]
                 for letter in marker:
                     if marker.count(letter) > 1:
                         index += 1
@@ -45,5 +48,6 @@ def step2():
                     break
 
     print("La réponse de l'étape 2 est :", start_of_message)
+
 
 step2()
